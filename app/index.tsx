212115a5d842db5e74c4e47bb41a4577927a449e
andresantos258 <130004923+andresantos258@users.noreply.github.com>
2024-08-17 01:02:36 +0000

    Aula02

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,64 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
 import { BorderlessButton } from "react-native-gesture-handler";
+import { useAnimatedKeyboard } from "react-native-reanimated";
+
+const produto = [
+  {id: 1, nome: "Cola-Cola", preco: 5.5 },
+  {id: 2, nome: "Pepsi", preco: 5.5 },
+  {id: 3, nome: "Fanta", preco: 5.5 },
+  {id: 4, nome: "Guaraná", preco: 5.5 }
+];
+
+
 
 export default function Index() {
+
+let [contador, setContador] = useState(0)
+let [fuga, setFuga] = useState(0)
+
   return (
     <View style={estilo.container}>
 
-      <Text style = {estilo.text}>Hello World!</Text>
+      {produto.map((p) => (
+        <View key={p.id}>
+
+        <Text style={estilo.text}>{p.nome} </Text>
+        <Text>{p.preco}</Text>
+
+        </View>)
+      )}
+      
+    <Button title={contador.toString()} onPress={()=>clicarBotao()}></Button>
 
     </View>
   );
-}
+
+  function clicarBotao(){
+    setContador(contador+1);
+  }
+
+  
+
+ }
+
+
 
 const estilo = StyleSheet.create({
   container: {
     flex:1,
       justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "#aabbcc"
+      alignItems: "flex-start",
+      backgroundColor: "#aabbcc",
+      paddingStart: 20
   },
     text: {
       color: "purple",
       fontSize: 30,
     },
+    titulo: {
+      color: "#FFFFFF",
+      fontSize: 30,
+      textAlign: "center"
+    }
 });
